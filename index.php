91510fddfac5f05b06cb8ff5ba8a348cbe369671
Rafael <m.bayu1405@gmail.com>
2026-02-20 11:26:07 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ayam</title>
+</head>
+<body>
+	<form method="post" action="process_index1.php">
+		Nama: <input type="text" name="name"><br>
+		Nomor: <input type="number" name="num"><br>
+		Password: <input type="text" name="pass"><br>
+		<input type="submit" value="submit">
+	</form>
+</body>
+</html>
+<?php
+
+?>
---
+++ process_index1.php
@@ -0,0 +1,15 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<form method="post" action="process_index1.php">
+		Nama: <input type="text" name="name"><br>
+		Password: <input type="text" name="pass"><br>
+		<input type="submit" value="submit">
+	</form>
+</body>
+</html>
